5417603cf33aff31d2ac37ef28fb1ff31e60e429
rivan <rivan_git@mail.ru>
2024-01-30 21:06:09 +0300

    change lesson 2 files

--- task_worker.py
+++ task_worker.py
@@ -1,4 +1,5 @@
 import asyncio
+
 from aio_pika import connect
 from aio_pika.abc import AbstractIncomingMessage
 
@@ -11,18 +12,26 @@ async def on_message(message: AbstractIncomingMessage) -> None:
 
 
 async def main() -> None:
+    # Perform connection
     connection = await connect("amqp://guest:guest@localhost/")
+
     async with connection:
+        # Creating a channel
         channel = await connection.channel()
-        queue = await channel.declare_queue("hello")
+        await channel.set_qos(prefetch_count=1)
+
         # Declaring queue
         queue = await channel.declare_queue(
             "task_queue",
             durable=True,
         )
+
+        # Start listening the queue with name 'task_queue'
         await queue.consume(on_message)
-        print(" [x] Waiting for messages. To exit press CTRL + C")
+
+        print(" [*] Waiting for messages. To exit press CTRL+C")
         await asyncio.Future()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
